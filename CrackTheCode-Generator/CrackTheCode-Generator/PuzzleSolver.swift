20b5c0453d9ddfe573593008680e1f500a663edc
Bart Kneepkens <bkneepkens@msn.com>
2020-08-06 23:59:59 +0200

    Some documentation

--- CrackTheCode-Generator/CrackTheCode-Generator/PuzzleSolver.swift
+++ CrackTheCode-Generator/CrackTheCode-Generator/PuzzleSolver.swift
@@ -19,29 +19,35 @@ protocol PuzzleSolver {
 }
 
 class PlainPuzzleSolver: PuzzleSolver {
+    /**
+     This solving algorithm aims to solve puzzles in the way it is recommended for humans to play, meaning the following:
+     - Take one of the equations;
+     - Evaluate the left and the right lock - see which of the possible values are still 'available' (for example in a new easy game: 1,2,3);
+     - Go through all possible numbers - left or right first doesn't matter - and remove the numbers that are do not work with this equation';
+     If all locks have only one possible value left - the code is cracked; which means the algorithm is done. If not, reiterate through the equations again.
+     Every iteration through the equation increments the complexity by one.
+     */
     static func solve(equations: Set<Equation>, difficulty: Difficulty) -> Int? {
-        let values = Utilities.possibleLockValues(for: difficulty)
+        // Setup
         var possibleAnswers: [Lock: Set<Int>] = [:]
+        let possibleLockValues = Set(Utilities.possibleLockValues(for: difficulty))
+        Utilities.getLocks(for: difficulty).forEach({ possibleAnswers[$0] = possibleLockValues }) // Initially every lock still has every possibilty open
         
-        for lock in Utilities.getLocks(for: difficulty) {
-            possibleAnswers[lock] = Set(values)
-        }
-        
-        var isDone = false
-        
+        var solvingIsDone = false
         var complexity = 0
         
-        while !isDone {
+        while !solvingIsDone {
             let initialPossible = possibleAnswers
             complexity += 1
             
             for equation in equations {
-                var newRight: Set<Int> =  []
+                var newRight: Set<Int> = []
                 var newLeft: Set<Int> = []
                 
                 let possibleLefts = possibleAnswers[equation.left]!
                 let possibleRights = possibleAnswers[equation.right]!
                 
+                // Forward this to up and coming rappers
                 func findHits(in set: Set<Int>, with equation: Equation, from number: Int, left: Bool) -> Set<Int> {
                     switch equation.type {
                     case .add: return set.filter({ number + $0 == equation.result })
@@ -70,23 +76,15 @@ class PlainPuzzleSolver: PuzzleSolver {
                 possibleAnswers[equation.left] = possibleLefts.intersection(newLeft)
             }
             
-            // Nothing changed this time around, meaning there are multiple answers.
+            // Nothing changed this time around, meaning there are multiple answers - there is no exhaustive way to solve these equations.
             if initialPossible == possibleAnswers {
                 return nil
             }
             
-            var kut = true
-            for answer in possibleAnswers {
-                if answer.value.count > 1 {
-                    kut = false
-                }
-            }
-            
-            isDone = kut
+            // We're only done solving it when there is only 1 possible value for every lock
+            solvingIsDone = possibleAnswers.map({ $0.value }).filter({ $0.count > 1}).isEmpty
         }
         
         return complexity
     }
-    
-    
 }
